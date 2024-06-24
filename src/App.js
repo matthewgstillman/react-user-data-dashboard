ec76569dede46cd97b261c618b0889c2956137d6
matthewgstillman <matthewgstillman@gmail.com>
2024-06-24 16:18:08 -0600

    UseEffect updates

--- src/App.js
+++ src/App.js
@@ -6,25 +6,32 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const response = await fetch("https://randomuser.me/api/");
-        if (!response.ok) {
-          throw new Error("Network reponse was not okay");
-        }
-        const data = await response.json();
-        console.log(data);
-        setUser(data.results[0]);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
+  const fetchUser = async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await fetch("https://randomuser.me/api/");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
       }
-    };
+      const data = await response.json();
+      console.log(data);
+      setUser(data.results[0]);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
+  useEffect(() => {
     fetchUser();
   }, []);
+
+  const formatDate = (dateString) => {
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
 
   if (loading) {
     return <div>Loading...</div>;
@@ -45,14 +52,27 @@ function App() {
           </h1>
           <div className="address">
             <p className="addressLine1">
-              {user.location.street.number} {user.location.street.name}{" "}
+              {user.location.street.number} {user.location.street.name}
+            </p>
+            <p className="addressLine2">
               {user.location.city}, {user.location.state}{" "}
               {user.location.postcode}
             </p>
-            <p className="addressLine2"></p>
+            <p className="email">
+              Email: <a href={`mailto:${user.email}`}>{user.email}</a>
+            </p>
+            <p className="dateOfBirth">
+              Date of Birth: {formatDate(user.dob.date)}
+            </p>
+            <p className="phoneNumber">
+              Cell phone: <a href={`tel:${user.cell}`}>{user.cell}</a>
+            </p>
+            <p className="userLoginInfo">Username: {user.login.username}</p>
           </div>
         </div>
       )}
+
+      <button onClick={fetchUser}>Get New Random User</button>
     </div>
   );
 }
